defe861832cbaee56ffe36883b8b3aa1d33fb919
Bayisa Daba <bayisa615@gmail.com>
2025-04-26 17:14:38 +0300

    add

--- selectionsort.py
+++ selectionsort.py
@@ -1,24 +1,29 @@
-class Solution {
-  public:
-    // Function to perform selection sort on the given array.
-    void selectionSort(vector<int> &arr) {
-        // code here
-                int n = arr.size();
-        
-        // Traverse through all elements in the array
-        for (int i = 0; i < n - 1; ++i) {
-            // Find the minimum element in the unsorted part of the array
-            int minIndex = i;
-            for (int j = i + 1; j < n; ++j) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
+class Solution:
+    # Function to perform selection sort on the given array
+    def selectionSort(self, arr):
+        n = len(arr)
+
+        # Traverse through all elements in the array
+        for i in range(n - 1):
+            # Find the minimum element in the unsorted part of the array
+            minIndex = i
+            for j in range(i + 1, n):
+                if arr[j] < arr[minIndex]:
+                    minIndex = j
             
-            // Swap the found minimum element with the element at index 'i'
-            if (minIndex != i) {
-                swap(arr[i], arr[minIndex]);
-            }
-        }
-    }
-};
+            # Swap the found minimum element with the element at index 'i'
+            if minIndex != i:
+                arr[i], arr[minIndex] = arr[minIndex], arr[i]
+
+
+# Driver code to test the Solution
+if __name__ == "__main__":
+    solution = Solution()
+    arr = [64, 25, 12, 22, 11]
+
+    print("Original array:", arr)
+
+    # Sort the array using selection sort
+    solution.selectionSort(arr)
+
+    print("Sorted array:", arr)
